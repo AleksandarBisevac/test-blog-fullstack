cbbb7253e6c3ff49d370a4c292f6fd6da2bee1ec
AleksandarBisevac <alek.bisevac@gmail.com>
2021-11-30 11:55:25 +0100

    added new pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticleList from './pages/ArticleList';
+import ArticlePage from './pages/ArticlePage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -8,6 +11,9 @@ function App() {
       <Router>
         <Routes>
           <Route exact path='/' element={<HomePage />} />
+          <Route path='/about' element={<AboutPage />} />
+          <Route path='/articles' element={<ArticleList />} />
+          <Route path='/article' element={<ArticlePage />} />
         </Routes>
       </Router>
     </div>
