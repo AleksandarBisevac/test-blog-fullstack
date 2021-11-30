3f755e492ebcdbca2d9b8f6c6bacb2cf4eae0d5d
AleksandarBisevac <alek.bisevac@gmail.com>
2021-11-30 11:39:15 +0100

    added react router dom library

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import './App.css';
+import HomePage from './pages/HomePage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<HomePage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <>
+      <h1>Home Page</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem quos
+        voluptate magnam, a sit tempore, error non nisi et architecto vitae
+        voluptatibus fugiat nostrum! Sunt eaque exercitationem amet ducimus
+        deserunt, obcaecati labore, neque, dicta culpa incidunt fugit quia
+        dolorem. Quis!
+      </p>
+      <p>
+        Corporis alias nihil amet ullam repellendus nostrum debitis nesciunt
+        ipsa dignissimos asperiores illo voluptas illum ipsum adipisci neque
+        officiis cumque mollitia est ab, sunt magnam quos quae fugit id. Nostrum
+        nesciunt quo doloribus magnam, necessitatibus quaerat harum! Animi,
+        explicabo iste.
+      </p>
+      <p>
+        Cum aperiam ab eaque impedit quae aspernatur accusamus illum dolorem
+        fuga, soluta voluptates ex quis mollitia. Ex facere doloribus officiis
+        consequuntur illo! Odio molestias nesciunt minima nemo culpa
+        dignissimos. Suscipit laborum accusantium ducimus recusandae optio
+        labore minima, inventore officiis quaerat?
+      </p>
+    </>
+  );
+};
+
+export default HomePage;
