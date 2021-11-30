4ad33b547e3118feaddd5b6e545eb3d6492c4796
AleksandarBisevac <alek.bisevac@gmail.com>
2021-11-30 12:08:21 +0100

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticleList from './pages/ArticleList';
 import ArticlePage from './pages/ArticlePage';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './Components/NavBar';
 
 function App() {
   return (
-    <div className='App'>
-      <Router>
+    <Router>
+      <div className='App'>
+        <NavBar />
         <Routes>
           <Route exact path='/' element={<HomePage />} />
           <Route path='/about' element={<AboutPage />} />
           <Route path='/articles' element={<ArticleList />} />
           <Route path='/article' element={<ArticlePage />} />
         </Routes>
-      </Router>
-    </div>
+      </div>
+    </Router>
   );
 }
 
