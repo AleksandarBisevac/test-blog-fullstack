6f7dc0b41de5494dd1408fdb2824d04aeaabacea
AleksandarBisevac <alek.bisevac@gmail.com>
2021-12-02 23:09:26 +0100

    fixed some bugs on article page, get rid off dummy data and added loader

--- src/pages/ArticleListPage.jsx
+++ src/pages/ArticleListPage.jsx
@@ -1,18 +1,23 @@
 import { useEffect, useState } from 'react';
 import ArticleList from '../Components/ArticleList';
+import Loader from '../Components/Loader';
 
 const ArticleListPage = () => {
   const [allArticles, setAllArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       const result = await fetch('/api/articles');
-      const body = await body.json();
+      const body = await result.json();
       setAllArticles(body);
+      setLoading(false);
     };
     fetchData();
   }, []);
-  return (
+  return loading ? (
+    <Loader />
+  ) : (
     <>
       <h1>Articles List</h1>
       <ArticleList articles={allArticles} />
--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -1,53 +1,63 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router';
-import articleContent from './article-content';
 import ArticleList from '../Components/ArticleList';
 import CommentsList from '../Components/CommentsList';
 import UpvotesSection from '../Components/UpvotesSection';
 import AddCommentForm from '../Components/AddCommentForm';
 import NotFoundPage from './NotFoundPage';
+import Loader from '../Components/Loader';
 
 const ArticlePage = () => {
   const { name } = useParams();
-  const article = articleContent.find(
-    (article) => article.name.toLowerCase() === name
-  );
-  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+  const [articleInfo, setArticleInfo] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [articles, setArticles] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchArticle = async () => {
       const result = await fetch(`/api/articles/${name}`);
       const body = await result.json();
       setArticleInfo(body);
     };
-    fetchData();
+    const fetchOtherArticles = async () => {
+      const result = await fetch(`/api/articles`);
+      const body = await result.json();
+      const otherArticles = body.filter(
+        (article) => article.name.toLowerCase() !== name
+      );
+      setArticles(otherArticles);
+    };
+    fetchArticle();
+    fetchOtherArticles();
+
+    setLoading(false);
+    window.scrollTo(0, 0);
   }, [name]);
 
-  if (!article) {
+  if (!loading && !articleInfo) {
     return <NotFoundPage />;
   }
-  const otherArticles = articleContent.filter(
-    (article) => article.name.toLowerCase() !== name
-  );
 
-  return (
+  return !loading && articles.length ? (
     <>
-      <h1>This is the {article.title} Article</h1>
+      <h1>This is the {articleInfo.title}</h1>
       <UpvotesSection
         upvotes={articleInfo.upvotes}
         articleName={name}
         setArticleInfo={setArticleInfo}
       />
-      {article.content.map((paragraph, idx) => (
+      {articleInfo.content.map((paragraph, idx) => (
         <p key={idx}>{paragraph}</p>
       ))}
       <CommentsList comments={articleInfo.comments} />
       <AddCommentForm setArticleInfo={setArticleInfo} articleName={name} />
       <section>
         <h3>Other Articles</h3>
-        <ArticleList articles={otherArticles} />
+        <ArticleList articles={articles} />
       </section>
     </>
+  ) : (
+    <Loader />
   );
 };
 
